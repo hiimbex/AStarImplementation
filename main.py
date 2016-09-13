89eb3e94d755ba25fbb896a35399e10800acf430
Bex Warner <rw1048a@student.american.edu>
2016-09-13 14:06:58 -0400

    point class added

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # Here is where our A Star Implementation should live :)
-input = [start,end]
+# Point Class code here:
+class Point:
+    xValue = 0
+    yValue = 0
+    def getCoord(self):
+        return (xValue, yValue)
+
+start = Point()
+start.xValue = 0
+start.yValue = 0
+
+input = [start, end, barriers]
 # Priority queues Open and Closed begin empty.
 open = []
 closed = []
@@ -10,10 +21,15 @@ open.append(input.pop(0))
 while open.length != 0:
     for x in range(start.x - 1, start.x + 1):
         for y in range(start.y - 1, start.y + 1):
-            open.append((x,y))
-    open.sort()
+            if (x, y) ! in open:
+                open.append((x,y))
+    open.sort() # by f values
 # Yes? No solution.
 # No. Remove node with lowest f(n). Call it n.
+    if open.pop() = input.end:
+        # goal reached!
+    else:
+        closed.append(open.pop(0))
 # If n is a goal, stop and report success (for pathfinding, return the path)
 # Otherwise, expand n. For each n’ in successors(n)
 # Let f’ = g(n’) + h(n’) = g(n) + cost (n, n’) + h(n’)
